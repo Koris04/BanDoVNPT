52c0e79f4324fe1a185c92001179dee802662bda
Thanh Phong <phongthanhnamky@gmail.com>
2026-03-09 14:27:27 +0700

    Add files via upload
    Update index trong routes

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,13 @@ router.get('/', (req, res) => {
     res.render('views', { title: 'Bản đồ giám sát mạng lưới', user: req.session.user || null });
 });
 
-// Route: API lấy điểm kết nối 
+//Route: API lấy điểm kết nối
 router.get('/api/diem-ket-noi', async (req, res) => {
     try {
-        const danhSachDiem = await DiemKetNoi.find({});
+        const danhSachDiem = await DiemKetNoi.find({}).populate('splitter_id');
         res.status(200).json(danhSachDiem);
     } catch (error) {
+        console.error("Lỗi API lấy điểm kết nối:", error);
         res.status(500).json({ message: "Lỗi máy chủ nội bộ" });
     }
 });
