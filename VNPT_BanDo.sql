e7fb86cdda09003a1eff2c49ddb71dd3e353066d
Koris04 <phongthanhnamky@gmail.com>
2026-05-07 19:53:01 +0700

    Cập nhật code SQL

--- VNPT_BanDo.sql
+++ VNPT_BanDo.sql
@@ -24,13 +24,6 @@ CREATE TABLE TaiKhoan (
     FOREIGN KEY (vai_tro_id) REFERENCES VaiTro(id),
     ly_do_khoa NVARCHAR(500)
 );
--- Thêm tài khoản
-INSERT INTO TaiKhoan (ten_dang_nhap, mat_khau, ho_ten, so_dien_thoai, dia_chi, vai_tro_id) 
-VALUES ('admin_phong', '123456', N'Đàm Thanh Phong', '0748119333', N'14AB Đặng Dung, Phường Long Xuyên', 1);
-INSERT INTO TaiKhoan (ten_dang_nhap, mat_khau, ho_ten, so_dien_thoai, dia_chi, vai_tro_id) 
-VALUES ('mane_phong', '123456', N'Đàm Thanh Phong', '0748119212', N'1 Đặng Dung, Phường Mỹ Thới', 2);
-INSERT INTO TaiKhoan (ten_dang_nhap, mat_khau, ho_ten, so_dien_thoai, dia_chi, vai_tro_id) 
-VALUES ('staff_phong', '123456', N'Đàm Thanh Phong', '0748119223', N'14AB Đặng Dung, Phường Bình Đức', 3);
 -- 3. Gói cước
 CREATE TABLE GoiCuoc (
     id INT IDENTITY(1,1) PRIMARY KEY,
